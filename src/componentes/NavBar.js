bcfef4ded3bc4ed98e9083084bf5125f0ea43439
Juan Pablo Santamarina <jpsantamarina@hotmail.com>
2022-07-28 20:37:07 -0300

    agregado de NavBar

---
+++ src/componentes/NavBar.js
@@ -0,0 +1,19 @@
+import "./NavBar.css";
+
+export default function NavBar (){
+    return(
+        <div className="menu" align="center">
+   <tr border="0">
+            <ul>
+                <li><a href="#">INICIO</a></li>
+                 <li><a href="#">PRODUCTOS</a></li>
+                 <li><a href="#">CONTACTO</a></li>
+                 <li><a href="#">NOSOTROS</a></li>
+            </ul>
+        </tr>
+        </div>
+     
+       
+    )
+}
+ 
